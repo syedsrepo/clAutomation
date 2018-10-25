9b03128658695373c791e348e906e9d548f0dd9b
syedsrepo <syed.salahuddin@outlook.com>
2018-10-25 11:21:27 +0530

    updated notes

--- demowebapp/runDemoWebApp.sh
+++ demowebapp/runDemoWebApp.sh
@@ -3,4 +3,9 @@ pip install flask
 dateval=`date`
 echo $dateval > /home/syeds/git/clAutomation/demowebapp/pip-info.log
 pip show flask >> /home/syeds/git/clAutomation/demowebapp/pip-info.log
-/usr/local/bin/demowebapp 5001
+/usr/local/bin/demowebapp 5001 &
+/usr/local/bin/demowebapp 5002 &
+/usr/local/bin/demowebapp 5003 &
+/usr/local/bin/demowebapp 5004 & 
+
+sleep infinity
