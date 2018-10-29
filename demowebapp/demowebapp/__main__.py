b998cf31968a11b354d8195173fc0f182e30f6f2
syedsrepo <syed.salahuddin@outlook.com>
2018-10-29 18:15:42 +0530

    updated main.py to include log and POST

--- demowebapp/demowebapp/__main__.py
+++ demowebapp/demowebapp/__main__.py
@@ -1,21 +1,40 @@
 from flask import Flask
 from flask import Response
 
+import logging
+from logging.handlers import RotatingFileHandler
+
 import socket
 import os
 import sys
 import time
 
 app = Flask(__name__)
+logger=logging.getLogger('demowebapp')
+logger.setLevel(logging.DEBUG)
+handler=logging.handlers.RotatingFileHandler('/var/log/demowebapp.log',maxBytes=10000000, backupCount=4)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index/get', methods=['GET'])
+def index_get():
+    retval = 'GET Request'
+    logger.debug('GET request received ' + socket.gethostname())
     hostname = socket.gethostname()
-    homedir = os.environ['HOME']
-    retval = hostname + ' ' + homedir
-    print retval
     time.sleep(.005)
+    logger.debug('GET request returning 200 response')
+    return Response(retval, status=200, mimetype='text/plain')
+
+
+@app.route('/index/post', methods=['POST'])
+def index_post():
+    retval = 'POST Request'
+    logger.debug('POST request received ' + socket.gethostname())
+    hostname = socket.gethostname()
+    time.sleep(.005)
+    logger.debug('POST request returning 200 response')
     return Response(retval, status=200, mimetype='text/plain')
 
 def main():
@@ -23,4 +42,4 @@ def main():
     app.run(host= '0.0.0.0', port=sys.argv[1], debug=True, threaded=True)
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=sys.argv[1], debug=True, threaded=True)
+    main()
